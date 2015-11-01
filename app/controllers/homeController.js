dc614cb95911433d8d121aa4faac5a22566aba52
Heli Wang <uwbook@outlook.com>
2015-11-01 15:10:12 -0500

    Add web api - full version

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,8 +1,9 @@
 ﻿'use strict';
-app.controller('homeController', ['$scope', '$log', '$routeParams', function ($scope, $log, $routeParams) {
+app.controller('homeController', ['$scope', '$log', '$http', '$routeParams', function ($scope, $log, $http, $routeParams) {
     console.log("Home Controller");
 
-   
+    var APIserver = "http://localhost:60219/";
+
     var user = $routeParams.user;
     $log.info('SomeCtrl home! user: ' + user + "Type: "+typeof user);
     if (typeof user != 'undefined' && user != "") $scope.authentication.userName = user; // localhost:32150/main.html#/home?user=moby&search=cs245
@@ -19,7 +20,14 @@ app.controller('homeController', ['$scope', '$log', '$routeParams', function ($s
 
     $scope.currentPage = 1;
     $scope.pageSize = 8;
-    $scope.books = [
+
+
+    $http.get(APIserver + "api/Books").success(function (response) {
+        $scope.books = response;
+        $log.info(response);
+    });
+
+    /* $scope.books = [
       {
           Name: "Calculus: Early Transcendentals 1E",
           ISBN: "1594633940",
@@ -28,8 +36,17 @@ app.controller('homeController', ['$scope', '$log', '$routeParams', function ($s
           Subjects: ["CS245"],
           Sellers: [{ Price: 32.5, Post: "20151028 10:41:52",  Note: "Never used, My phone number 2269787328" , Contact: "h379wang@uwaterloo.ca", Hot: 2 },
                     { Price: 16.8, Post: "20151028 10:41:53", Note: "Used, My phone number 2269787328", Contact: "h379wang@uwaterloo.ca", Hot:3 }]
-      }
-    ];
+      },
+            {
+                Name: "Calculus: Early Transcendentals 2E",
+                ISBN: "1594633940",
+                Author: "Marlon James",
+                CoverURL: "http://i.ebayimg.com/00/s/NDAwWDI2NA==/z/hi0AAOSw-ndTndXU/$_7.JPG?set_id=89040003C1",
+                Subjects: ["CS245"],
+                Sellers: [{ Price: 32.5, Post: "20151028 10:41:52", Note: "Never used, My phone number 2269787328", Contact: "h379wang@uwaterloo.ca", Hot: 2 },
+                          { Price: 16.8, Post: "20151028 10:41:53", Note: "Used, My phone number 2269787328", Contact: "h379wang@uwaterloo.ca", Hot: 3 }]
+            }
+    ]; */
 
 
     $scope.pageChangeHandler = function (num) {
